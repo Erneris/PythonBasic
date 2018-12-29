2233ca427f8216ed133f5694db8cbe1dcd3c2f56
erneris <ernestassin1@gmail.com>
2018-12-29 21:18:24 +0200

    Pirmas upload is home pc

--- Nieko.py
+++ Nieko.py
@@ -1,23 +1,34 @@
-
-
-print('Ivesk primą skiačiu')
-p = input()
-print('Ivesk antra skiačiu')
-a = input()
-print('Iveskite simboli')
+﻿print('Iveskite simboli')
 s = input()
+if(s == '*2'):
+    print('Ivesk skaičiu')
+    p = input()
+elif (s == '*3'):
+    print('Ivesk skaičiu')
+    p = input()
+else:
+    print('Ivesk pirmą skiačiu')
+    p = input()
+    print('Ivesk antra skiačiu')
+    a = input()
 
 if(s == '+'):
       sum = int(p) + int(a)
       print(sum)
 elif(s == '-'):
-     sum = int(p)- int(a)
+     sum = int(p) - int(a)
      print(sum)
 elif(s == '/'):
      sum = int(p) / int(a)
      print(sum)
 elif(s == '*'):
      sum = int(p) * int(a)
+     print(sum)
+elif(s == '*2'):
+     sum = int(p) * int(p)
+     print(sum)
+elif (s == '*3'):
+     sum = int(p) * int(p) * int(p)
      print(sum)
 else:
     print("Neteisingas simbolis")
