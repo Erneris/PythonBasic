4c606a10b57a19a9ca351b30558db9516cfc9351
unknown <you@example.com>
2019-01-09 15:42:03 +0200

    Šaknis + Bsk GUI

--- Nieko.py
+++ Nieko.py
@@ -1,10 +1,36 @@
-﻿print('Iveskite simbolį')
+﻿from math import sqrt
+
+from tkinter import *
+
+
+def add_label():
+    label_1=Label(my_window, text = "Hello World")
+    label_1.pack()
+my_window = Tk()
+my_window.title  ("Test")
+my_window.configure (bg='black')
+my_window.geometry("1400x900")
+
+button_1 = Button(my_window,
+                  text="Click me",
+                  command=add_label)
+
+button_1.pack()
+
+my_window.mainloop()
+
+
+print('Iveskite simbolį')
 s = input()
 if (s == '^'):
     print('Ivesk skaičių')
     p = float(input())
     print('Ivesk laipsnį')
     l = float(input())
+
+elif (s == 's'):
+    print('Ivesk skaičių')
+    p = float(input())
 
 else:
     print('Ivesk pirmą skiačių')
@@ -22,6 +48,8 @@ elif (s == '*'):
     print(p*a)
 elif (s == '^'):
     print(pow(p, l))
-
+elif (s == 's'):
+    print(sqrt(p))
 else:
     print("Neteisingas simbolis")
+#Made_by_Erneris
