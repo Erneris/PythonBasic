e0922bd8c3ad7cd19e3f39758ef50724b43040e3
unknown <you@example.com>
2019-01-23 15:38:00 +0200

    Buttons ir fix

--- Nieko.py
+++ Nieko.py
@@ -1,21 +1,62 @@
 ﻿from math import sqrt
 
+#https://stackoverflow.com/questions/10927234/setting-the-position-on-a-button-in-python
 from tkinter import *
+from tkinter import Button
 
-
-def add_label():
-    label_1=Label(my_window, text = "Hello World")
-    label_1.pack()
 my_window = Tk()
 my_window.title  ("Test")
 my_window.configure (bg='black')
 my_window.geometry("1400x900")
 
-button_1 = Button(my_window,
-                  text="Click me",
-                  command=add_label)
+button_plus = Button(my_window,text="+",width = 10)
+
+button_plus.pack()
+
+button_1 = Button(my_window,text="1",width = 10)
+#button_1.grid(row=0, column=0)
 
 button_1.pack()
+
+button_2 = Button(my_window,text="2",width = 10)
+#button_2.grid(row=1, column=0)
+
+button_2.pack()
+
+button_3 = Button(my_window,text="3",width = 10)
+
+button_3.pack()
+
+button_4 = Button(my_window,text="4",width = 10)
+
+button_4.pack()
+
+button_5 = Button(my_window,text="5",width = 10)
+
+button_5.pack()
+
+button_6 = Button(my_window,text="6",width = 10)
+
+button_6.pack()
+
+button_7 = Button(my_window,text="7",width = 10)
+
+button_7.pack()
+
+button_8 = Button(my_window,text="8",width = 10)
+
+button_8.pack()
+
+button_9 = Button(my_window,text="9",width = 10)
+
+button_9.pack()
+
+button_0 = Button(my_window,text="0",width = 10)
+
+button_0.pack()
+
+
+
 
 my_window.mainloop()
 
